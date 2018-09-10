3fd85d5061c3bda62fec0edbdcf7f86747281fe9
Shahriar <ssha7332@gmail.com>
2018-09-10 11:37:29 -0600

    Created Repository

---
+++ src/controller/FirstController.java
@@ -0,0 +1,11 @@
+package controller;
+
+public class FirstController
+{
+
+	public void start()
+	{
+		System.out.println("Words go here");
+
+	}	
+}	
---
+++ src/controller/StupidRunner.java
@@ -0,0 +1,10 @@
+package controller;
+
+public class StupidRunner
+{
+	public static void main (String [] args)
+	{
+		FirstController app = new FirstController();
+		app.start();
+	}
+}
